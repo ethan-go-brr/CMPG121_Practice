0509e4d074e982cf1630afd2d27e1c2f107af3a4
Ethan Govender <ethangovender31342018@outlook.com>
2026-09-08 12:43:26 +0200

    Changed /0 to \0 (null terminator)

--- string_handling/c_style_strings_pt2.cpp
+++ string_handling/c_style_strings_pt2.cpp
@@ -31,7 +31,7 @@ void addContact(Contact ct[], int& count)
 
 void toLowerCopy(char str[])
 {
-    for(int i = 0; str[i] != '/0'; i++)
+    for(int i = 0; str[i] != '\0'; i++)
     {
         str[i] = tolower(str[i]);
     }
